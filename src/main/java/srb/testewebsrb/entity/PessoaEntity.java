a0f02eb7f5016fe612caeb6671f5bbf86848e09c
Sandro Roger Boschetti <sandrorb@gmail.com>
2022-08-15 20:56:19 -0300

    Incorporação do Postgresql e outras mudanças menores.

---
+++ src/main/java/srb/testewebsrb/entity/PessoaEntity.java
@@ -0,0 +1,30 @@
+package srb.testewebsrb.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PessoaEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public long id;
+	public String nome;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
