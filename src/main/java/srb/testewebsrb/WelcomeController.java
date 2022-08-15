486be30b49a5bc81071f05fdf7217b125ab18560
Sandro Roger Boschetti <sandrorb@gmail.com>
2022-08-15 17:09:59 -0300

    Comentário no versão Java e menor mudança um controller.

--- src/main/java/srb/testewebsrb/WelcomeController.java
+++ src/main/java/srb/testewebsrb/WelcomeController.java
@@ -13,19 +13,4 @@ public class WelcomeController {
 
 }
 
-/*
-package br.com.srb.testeweb;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class WelcomeController {
-	
-	@GetMapping("/welcome")
-	public String welcome() {
-		return "Bem-vindo, Sandro!";
-	}
-}
-*/
 
