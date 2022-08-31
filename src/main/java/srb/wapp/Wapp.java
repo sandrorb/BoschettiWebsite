69022dbacaef0c1adef473b8c504ef11f72465d3
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-08-31 11:21:36 -0300

    Webservice básico de teste Twilio/Whatsapp

--- src/main/java/srb/wapp/Wapp.java
+++ src/main/java/srb/wapp/Wapp.java
@@ -1,6 +1,5 @@
 package srb.wapp;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,9 @@ public class Wapp {
 	
 	@PostMapping(value = "/wapp")
 	public String resposta(@RequestParam String From, @RequestParam String Body) {
+		String numTelefone = From.substring(From.indexOf(":")+1);
 		StringBuilder sb = new StringBuilder();
-		sb.append("Olá, " + From + "!");
+		sb.append("Olá, " + numTelefone + "!\n");
 		sb.append("Você digitou: " + Body);
 		return sb.toString();
 	}
