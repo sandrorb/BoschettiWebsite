0561bba672d54dc0058f7445d9b06d1f2b372789
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-08-31 16:05:33 -0300

    Adição de um endpoint para envio de msg

--- src/main/java/srb/wapp/Wapp.java
+++ src/main/java/srb/wapp/Wapp.java
@@ -1,8 +1,15 @@
 package srb.wapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import srb.MyProperties;
 
 @RestController
 public class Wapp {
@@ -15,6 +22,26 @@ public class Wapp {
 		sb.append("Esta é uma mensagem automática criada e enviada por um webservice escrito em Java por Sandro Boschetti\n");
 		sb.append("Você digitou: " + Body);
 		return sb.toString();
+	}
+	
+	
+	@GetMapping(value = "/enviaMsg")
+	public void enviaMsg(@RequestParam String accountSid, @RequestParam String authToken, 
+			             @RequestParam String phoneNumberOriginStr, @RequestParam String phoneNumberDestStr) {
+		
+		Twilio.init(accountSid, authToken);	
+		
+		phoneNumberOriginStr = "whatsapp:" + phoneNumberOriginStr;
+		  phoneNumberDestStr = "whatsapp:" + phoneNumberDestStr;
+		
+		PhoneNumber phoneNumberOrigin = new PhoneNumber(phoneNumberOriginStr);
+		PhoneNumber phoneNumberDest = new PhoneNumber(phoneNumberDestStr);
+	
+		String body = "SrB: Isso é um teste";
+		 
+		Message msg = Message.creator( phoneNumberOrigin, phoneNumberDest, body).create();
+		
+		System.out.println(msg.getSid());				
 	}
 
 }
