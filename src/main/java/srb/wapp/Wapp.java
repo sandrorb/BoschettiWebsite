a37744e4d9273b79500ba352570a6c1523fedce8
Sandro Roger Boschetti <sandro.boschetti@ebserh.gov.br>
2022-08-31 16:17:44 -0300

    Adição de um endpoint para envio de msg

--- src/main/java/srb/wapp/Wapp.java
+++ src/main/java/srb/wapp/Wapp.java
@@ -9,8 +9,6 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
-import srb.MyProperties;
-
 @RestController
 public class Wapp {
 	
